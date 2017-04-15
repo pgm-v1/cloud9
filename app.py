871e359a9c4e76a6150ffca8a7794694835a8e7c
John Smith <pgm-v1@hotmail.com>
2017-04-15 17:59:48 +0000

    fix

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # -*- coding:utf-8 -*-
 
 import os
-from bottle import route, run
+from bottle import route, run, template
 
+@route("/")
+def index():
+        return template('index')
 
-@route('/hello')
-def hello():
-    return "Hello World !!"
-
+@route("/<names>")
+def others(names):
+        return template('404', names=names)
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
