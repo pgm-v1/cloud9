c8a67faab3cea53a66402f9da0d66ccd4a2f9683
John Smith <pgm-v1@hotmail.com>
2017-04-15 18:05:08 +0000

    add 404

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from bottle import route, run, template
 
 @route("/")
 def index():
-        return template('index')
+    return template('index')
 
-@route("/<names>")
-def others(names):
-        return template('404', names=names)
+#@route("/<names>")
+#def others(names):
+#    return template('404', names=names)
+
+@error(404)
+def error404(error):
+    return "Not Found!"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
