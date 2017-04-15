695054e0b8cef0681f14206ab705838fbfe1ff0c
John Smith <pgm-v1@hotmail.com>
2017-04-15 18:21:18 +0000

    fix

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # -*- coding:utf-8 -*-
 
 import os
-from bottle import route, run
+from bottle import route, run, template, error
 
+@route("/")
+def index():
+        return template('index')
 
-@route('/hello')
-def hello():
-    return "Hello World !!"
-
+@error(404)
+def error404(error):
+    return "Not Found!"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
